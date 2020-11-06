f2156fb845f5d82f9ffa310dd8e103c4395fbe88
Yuzyzy88 <>
2020-11-06 03:02:18 -0500

    feat: check login when visiting site first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { AnonymouseHome } from "../components/home_anonymous";
 import { LoggedInHome } from "../components/home_loggedin";
 import { RootState } from "../state/reducers";
+import { useThunkDispatch } from "../state/store";
+import { check_login } from "../state/userSlice";
 
 export function Home() {
   const { logged_in } = useSelector((state: RootState) => state.user);
+  const thunkDispatch = useThunkDispatch()
+  useEffect(() => {
+    thunkDispatch(check_login());
+  },[])
   return <>{logged_in ? <LoggedInHome /> : <AnonymouseHome />}</>;
 }
 export default Home;
